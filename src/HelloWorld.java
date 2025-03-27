f494c21dcc17f571b5a8e533ac2ceb1e5e31f3ad
wlstjr2867 <wlstjr2867@naver.com>
2025-03-27 17:48:27 +0900

    HelloWorld프린트문추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("이클립스 첫 실행");
 		System.out.println("Hello Eclipse");
+		System.out.println("Github연동완료");
 	}
 }
