8a995b61f9edccaf4aa53abca513af9e2939dfaf
wlstjr2867 <wlstjr2867@naver.com>
2025-03-28 16:42:43 +0900

    학원커밋

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("1.Hello Eclipse");
 		System.out.println("2.Github연동완료");
 		System.out.println("3.집에서 작업");
+		System.out.println("4.학원에서 작업");
 	}
 }
