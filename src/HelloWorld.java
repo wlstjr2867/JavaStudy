2c1334f85714cb106a7c68c55012c91d26c58abd
wlstjr2867 <wlstjr2867@naver.com>
2025-03-28 16:19:47 +0900

    집에서작업

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Eclipse");
-		System.out.println("Github연동완료");
+		System.out.println("1.Hello Eclipse");
+		System.out.println("2.Github연동완료");
+		System.out.println("3.집에서 작업");
 	}
 }
--- src/test/ex08class/PersonMain.java
+++
@@ -1,9 +0,0 @@
-package test.ex08class;
-
-public class PersonMain {
-
-	public static void main(String[] args) {
-		Person p = new Person();
-	}
-
-}
